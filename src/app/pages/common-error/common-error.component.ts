54d9ab291c18d96bdfdd5fb0eef37fb1516acafb
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-09-24 11:28:47 +0530

    implement and design page 404

--- src/app/pages/common-error/common-error.component.ts
+++ src/app/pages/common-error/common-error.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ConfigPrimeModule } from '../../config-prime/config-prime.module';
 
 @Component({
   selector: 'app-common-error',
   standalone: true,
-  imports: [],
+  imports: [ConfigPrimeModule],
   templateUrl: './common-error.component.html',
-  styleUrl: './common-error.component.css'
+  styleUrl: './common-error.component.css',
 })
-export class CommonErrorComponent {
-
-}
+export class CommonErrorComponent {}
