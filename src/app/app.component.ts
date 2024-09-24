634cb011ef601b3b27389147286ef87beef2abca
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-09-24 11:18:06 +0530

    implement menubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConfigPrimeModule } from './config-prime/config-prime.module';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,28 @@ import { ConfigPrimeModule } from './config-prime/config-prime.module';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'traveller-post';
+  items: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Features',
+        icon: 'pi pi-star',
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+      },
+    ];
+  }
 }
--- src/app/config-prime/config-prime.module.ts
+++ src/app/config-prime/config-prime.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-
+import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
 @NgModule({
   declarations: [],
-  exports: [ButtonModule],
+  exports: [ButtonModule, MenubarModule, InputTextModule, AvatarModule],
 })
 export class ConfigPrimeModule {}
