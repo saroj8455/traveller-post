6c45d34ccf89057c43883cd3dcbdc18d728c1e3c
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-10-06 08:20:02 +0530

    update basic imple in authguard

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,10 +1,19 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from './services/user.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const userService = inject(UserService);
-  return userService.isAuthenticated('admin');
+  const router = inject(Router);
+
+  const verifyPermission = userService.isAuthenticated('admin');
+
+  if (!verifyPermission) {
+    router.navigate(['welcome']);
+    return false;
+  }
+
+  return verifyPermission;
 
   // return false;
 };
